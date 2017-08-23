d5b521062e78f94db963dcd9bf88ad40a5bd13df
Quaxxe <31284789+Quaxxe@users.noreply.github.com>
2017-08-23 18:38:55 +0200

    Vereinfacht

--- Test1/Test1.ino
+++ Test1/Test1.ino
@@ -1,39 +1,32 @@
 
- 
+
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 13;
-int state = 0;
-int flag = 0;
+
 
 
 // the setup routine runs once when you press reset:
-void setup() {               
-  pinMode(led, OUTPUT); 
+void setup() {
+  pinMode(led, OUTPUT);
   digitalWrite (led, LOW);
   Serial.begin (9600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  if(Serial.available()>0){
-    state = Serial.read();
-    flag = 0;
-  }
-  
-  if (state == '0'){
-  digitalWrite(led, LOW);   
-  if (flag =='0'){
-    Serial.println("LED: off");
-    flag=1;
-  }    
-}
+  char input;
 
-else if (state =='1'){
-    digitalWrite(led, HIGH);   
-    if (flag =='0'){
-    Serial.println("LED: on");
-    flag=1;
+  if (Serial.available() > 0) {
+    input = Serial.read();
+    if (input == '0') {
+      digitalWrite (led, LOW);
+    }
+    else if (input == '1') {
+      digitalWrite (led, HIGH);
+    }
+    else {
+      Serial.println ("Bitte 0 oder 1");
     }
   }
-}  
+}

c688973b75f490ddebdc937a62cd4184cc9507a4
Quaxxe <31284789+Quaxxe@users.noreply.github.com>
2017-08-23 20:00:27 +0200

    Kommunikation mit BT in beide Richtungen

--- Test1/Test1.ino
+++ Test1/Test1.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial btSerial(2, 3); // RX, TX
 
 
 // Pin 13 has an LED connected on most Arduino boards.
@@ -11,22 +14,24 @@ void setup() {
   pinMode(led, OUTPUT);
   digitalWrite (led, LOW);
   Serial.begin (9600);
+  btSerial.begin (115200);
+  btSerial.write ("---");
+  btSerial.begin (9600);
+
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   char input;
 
+  if (btSerial.available() > 0) {
+    input = btSerial.read();
+    Serial.write(input);
+  }
   if (Serial.available() > 0) {
     input = Serial.read();
-    if (input == '0') {
-      digitalWrite (led, LOW);
-    }
-    else if (input == '1') {
-      digitalWrite (led, HIGH);
-    }
-    else {
-      Serial.println ("Bitte 0 oder 1");
-    }
+    btSerial.write(input);
+    Serial.write(input);
   }
+
 }
